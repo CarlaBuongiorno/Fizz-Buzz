59d1423bb8a92f1d6b7e086a34cfeb221322c9e2
Carla Buongiorno <carla.buongiorno@dataexpert.nl>
2025-02-06 10:43:18 +0100

    Tests 'Fizz Buzz' is returned for multiples of 3 and 5.

--- test_fizz_buzz.py
+++ test_fizz_buzz.py
@@ -4,7 +4,7 @@ from fizz_buzz import fizz_buzz
 def test_fizz_buzz_exists():
     assert fizz_buzz
 
-def test_fizz():
+def test_fizz_for_multiple_of_3():
     fizz = fizz_buzz(3)
     assert fizz == 'Fizz'
 
@@ -12,6 +12,10 @@ def test_no_fizz_return_number():
     number = fizz_buzz(1)
     assert number == 1
 
-def test_buzz():
+def test_buzz_for_multiple_of_5():
     buzz = fizz_buzz(5)
     assert buzz == 'Buzz'
+
+def test_fizz_buzz_for_multiple_of_3_and_5():
+    fizzbuzz = fizz_buzz(15)
+    assert fizzbuzz == 'Fizz Buzz'
