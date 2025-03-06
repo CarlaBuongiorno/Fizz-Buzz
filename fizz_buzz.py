fa6aef4f3ef34d255030df297cfe6aa46349e680
Carla Buongiorno <carla.buongiorno@dataexpert.nl>
2025-03-06 13:12:08 +0100

    Add single fizz_buzz() function call to fizz_buzz.py

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -3,16 +3,20 @@
 # https://en.wikipedia.org/wiki/Fizz_buzz
  
 from pure_fizz_buzz import (
-   get_number,
-   fizz_buzz_checker,
-   fizz_buzz_list,
+#    get_number,
+#    fizz_buzz_checker,
+#    fizz_buzz_list,
+   fizz_buzz,
   )
 
 def main():
-  number = get_number()
-  number_list = fizz_buzz_list(number)
-  fizzbuzz_list = fizz_buzz_checker(number_list)
-  print(fizzbuzz_list)
+#   number = get_number()
+    number = int(input("Pick a number for FizzBuzz from 1 to 50: "))
+    fizzbuzz = fizz_buzz(number)
+    print(fizzbuzz)
+    # number_list = fizz_buzz_list(number)
+    # fizzbuzz_list = fizz_buzz_checker(number_list)
+    # print(fizzbuzz_list)
 
 
 if __name__ == "__main__":
