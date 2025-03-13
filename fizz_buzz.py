0bb45cdd64f9adb268f56efcac07a37eedc26bb7
Carla Buongiorno <carla.buongiorno@dataexpert.nl>
2025-03-13 15:09:21 +0100

    Amalgamate code into one version

--- fizz_buzz.py
+++ fizz_buzz.py
@@ -3,20 +3,15 @@
 # https://en.wikipedia.org/wiki/Fizz_buzz
  
 from pure_fizz_buzz import (
-#    get_number,
-#    fizz_buzz_checker,
-#    fizz_buzz_list,
+   get_number,
    fizz_buzz,
   )
 
+
 def main():
-    # number = get_number()
-    number = int(input("Pick a number for FizzBuzz from 1 to 50: "))
+    number = get_number()
     fizzbuzz = fizz_buzz(number)
     print(fizzbuzz)
-    # number_list = fizz_buzz_list(number)
-    # fizzbuzz_list = fizz_buzz_checker(number_list)
-    # print(fizzbuzz_list)
 
 
 if __name__ == "__main__":
--- pure_fizz_buzz.py
+++ pure_fizz_buzz.py
@@ -1,74 +1,41 @@
-# import random
+import random
+
+
+def get_number():
+    return random.randrange(1,50)
 
 
 def fizz_buzz(number):
-    '''Single fizz_buzz() function'''
     fizzbuzz = list(range(1, number + 1))
-    for index, number in enumerate(fizzbuzz):
-        if number % 5 == 0 and number % 3 == 0:
-            fizzbuzz[index] = 'FizzBuzz'
-        elif number % 3 == 0:
-            fizzbuzz[index] = 'Fizz'
-        elif number % 5 == 0:
-            fizzbuzz[index] = 'Buzz'
-        else:
-            fizzbuzz[index] = str(number)
-    return fizzbuzz
+    number_list = fizz_buzz_checker(fizzbuzz)
+    fizzbuzz_list = fizz_buzz_list(number_list)
+    return fizzbuzz_list
 
 
-# def fizz_buzz(number):
-    '''Single fizz_buzz() function alternative 1'''
-#     fizz_buzz_list = []
-#     for i in range(1, number + 1):
-#         if i % 5 == 0 and i % 3 == 0:
-#             fizz_buzz_list.append(f'FizzBuzz')
-#         elif i % 3 == 0:
-#             fizz_buzz_list.append(f'Fizz')
-#         elif i % 5 == 0:
-#             fizz_buzz_list.append(f'Buzz')
-#         else:
-#             fizz_buzz_list.append(str(i))
-#     return fizz_buzz_list
+def fizz_buzz_checker(number_list):
+    CHECKS = ((fizz_and_buzz, 'Fizz Buzz'),
+              (fizz, 'Fizz'),
+              (buzz, 'Buzz'),)
+
+    for index, (number) in enumerate(number_list):
+        for func, txt in CHECKS:
+            if func(int(number)):
+                number_list[index] = txt
+                break
+    return number_list
 
 
-# def fizz_buzz(n):
-    '''Single fizz_buzz() function alternative 2'''
-#     return [
-#         "FizzBuzz" if i % 3 == 0 and i % 5 == 0 else "Fizz" if i % 3 == 0 else "Buzz" if i % 5 == 0 else str(i)
-#         for i in range(1, n + 1)
-#     ]
-
-'''============================================================='''
-
-'''Separate into small functions'''
-# def get_number():
-#     return random.randrange(1,50)
+def fizz_and_buzz(number):
+    return number % 5 == 0 and number % 3 == 0
 
 
-# def fizz_buzz_list(number):
-#     return [str(n) for n in range(1, number + 1)]
+def fizz(number):
+    return number % 3 == 0
 
 
-# def fizz_buzz_checker(number_list):
-#     CHECKS = ((fizz_buzz, 'Fizz Buzz'),
-#               (fizz, 'Fizz'),
-#               (buzz, 'Buzz'),)
-
-#     for index, (number) in enumerate(number_list):
-#         for func, txt in CHECKS:
-#             if func(int(number)):
-#                 number_list[index] = txt
-#                 break
-#     return number_list
+def buzz(number):
+    return number % 5 == 0
 
 
-# def fizz_buzz(number):
-#     return number % 5 == 0 and number % 3 == 0
-
-
-# def fizz(number):
-#     return number % 3 == 0
-
-
-# def buzz(number):
-#     return number % 5 == 0
+def fizz_buzz_list(number_list):
+    return [str(n) for n in number_list]
